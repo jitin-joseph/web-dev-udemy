587e10982c8b2c6387cffeb5facdb66e4922d5d0
jitin-joseph <62872551+jitin-joseph@users.noreply.github.com>
2024-09-30 20:27:36 +0530

    js printing

---
+++ jsfoundation/part1/printing.js
@@ -0,0 +1,11 @@
+console.log("Hello"); //Hello
+
+console.table({"city":"Kochi"}); 
+/*
+┌─────────┬─────────┐
+│ (index) │ Values  │
+├─────────┼─────────┤
+│ city    │ 'Kochi' │
+└─────────┴─────────┘*/
+
+console.warn({"city":"Kochi"}); //{ city: 'Kochi' }
