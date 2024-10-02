434c72330ca93580d22bd46aac4fd3ceb16b06e9
jitin-joseph <62872551+jitin-joseph@users.noreply.github.com>
2024-10-02 10:24:13 +0530

    datatype

---
+++ jsfoundation/part1/datatype.js
@@ -0,0 +1,23 @@
+
+/*
+String --> jitin, php
+Number --> 3,4,5,305,164.8
+Boolean --> true or false
+Bigint --> bigger numbers manily for scientific data
+
+Undefined --> I have no definition yet, there might be dfinition in future I can't predict it
+null --> empty, nothing
+
+Object
+
+Symbol
+
+*/
+
+let score = 102; //Number
+let name = "Jitin"; //String
+let isLoggedIn = true; //Boolean
+
+//Object
+let teaTypes=["ginger tea","lemon tea","black tea","green tea"];
+let user ={firstname:"Jitin",lastname:"Joseph"};
