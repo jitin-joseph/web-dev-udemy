2664e25c1071ee72a7c690f0d71cb8f0ab54e786
jitin-joseph <62872551+jitin-joseph@users.noreply.github.com>
2024-10-02 17:34:19 +0530

    operations

---
+++ jsfoundation/part1/operations.js
@@ -0,0 +1,34 @@
+let addition = 4+5; 
+console.log(addition);//9
+
+let subtraction = 9-3; 
+console.log(subtraction);//6
+
+let multiplication = 6*7; 
+console.log(multiplication);//42
+
+let division = 8/2; 
+console.log(division);//4
+
+let reminder = 9%2; 
+console.log(reminder);//1
+
+let exponent = 2**5; //2^5
+console.log(exponent);//32
+
+let myscore=110;
+myscore++;
+console.log(myscore);//111
+
+let credits=56;
+credits--;
+console.log(credits);//55
+
+//comparison
+let num1=3; let num2=3; let num3=6;
+
+console.log(num1==num2);//true
+console.log(num1!=num2);//false
+console.log(num1==num3);//false
+console.log(num1<num3);//true
+console.log(num1>num3);//false
