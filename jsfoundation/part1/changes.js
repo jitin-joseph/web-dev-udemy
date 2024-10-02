d05a9641b647451526e153cbbe14c4addceecf08
jitin-joseph <62872551+jitin-joseph@users.noreply.github.com>
2024-10-02 17:20:31 +0530

    datatype changes

---
+++ jsfoundation/part1/changes.js
@@ -0,0 +1,13 @@
+let gameName = "Fifa 2024";
+gameName = "Call of Duty";
+
+console.log("gameName "+gameName);
+//gameName Call of Duty
+
+
+const username = "jitinjoseph";
+usrname = "jitin1711";
+
+console.log("username "+username);
+//username jitinjoseph
+//const variables doesn't changes
